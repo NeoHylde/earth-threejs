9fcc5c6e7f9515f1f8bad15f88bbd5d9e40f23e6
Neo <neo.hyldelund@gmail.com>
2025-08-25 12:44:15 -0700

    pins can be added with real world lat and lon coordinates

--- conflict-mapper/app/components/EarthMesh.tsx
+++ conflict-mapper/app/components/EarthMesh.tsx
@@ -4,19 +4,43 @@ import * as THREE from "three"
 import { Canvas, useFrame, useLoader } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import { Mesh } from "three";
+import PinMesh from "./PinMesh";
+import { degToRad } from "three/src/math/MathUtils.js";
+
+const SPIN_RATE = 0.3;
+const RADIUS = 4;
+
+function latLonToCartesian(latDeg: number, lonDeg: number, r = RADIUS + 0.15) {
+  const lat = THREE.MathUtils.degToRad(latDeg);
+  const lon = THREE.MathUtils.degToRad(lonDeg);
+  const x = r * Math.cos(lat) * Math.cos(lon);
+  const y = r * Math.sin(lat);
+  const z = -r * Math.cos(lat) * Math.sin(lon);
+  return [x, y, z] as [number, number, number];
+}
 
 export default function EarthMesh() {
-    const ref = React.useRef<Mesh>(null!);
+    const earthGroupRef = React.useRef<Mesh>(null!);
     const map = useLoader(THREE.TextureLoader, "./textures/earth-daymap-4k.jpg")
 
-    useFrame(() => {
-        ref.current.rotation.x += 0.00;
-        ref.current.rotation.y += 0.01;
+    const vanPin = latLonToCartesian(49.28, -123.12);
+    const copPin = latLonToCartesian(55.67, 12.57);
+    const zeroPin = latLonToCartesian(0,0);
+
+    useFrame((_, delta) => {
+    earthGroupRef.current.rotation.y += SPIN_RATE * delta;
+    earthGroupRef.current.rotation.x = 0.3;
     });
+
     return (
-        <mesh ref={ref}>
-            <icosahedronGeometry args={[4, 5]}/>
-            <meshStandardMaterial map={map}/>
-        </mesh>
+            <group ref={earthGroupRef}>
+                <mesh rotation={[0, degToRad(180), 0]}>
+                    <icosahedronGeometry args={[RADIUS, 5]} />
+                    <meshStandardMaterial map={map} />
+                </mesh>
+                <PinMesh position={vanPin} />
+                <PinMesh position={copPin} />
+                <PinMesh position={zeroPin} />
+            </group>
     );
 };
--- conflict-mapper/app/components/PinMesh.tsx
+++ conflict-mapper/app/components/PinMesh.tsx
@@ -5,20 +5,36 @@ import { Canvas, useFrame, useLoader } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import { Mesh } from "three";
 import { group } from "console";
-import { radToDeg } from "three/src/math/MathUtils.js";
+import { degToRad, radToDeg } from "three/src/math/MathUtils.js";
+import { PI } from "three/tsl";
 
-export default function PinMesh() {
-    const ref1 = React.useRef<Mesh>(null!);
-    const ref2 = React.useRef<Mesh>(null!);
+type PinMeshProps = {
+    position?: [number, number, number];
+}
+
+export default function PinMesh({position = [0,0,0]}: PinMeshProps) {
+    const groupRef = React.useRef<THREE.Group>(null!);
+    const refHead = React.useRef<Mesh>(null!);
+    const refCone = React.useRef<Mesh>(null!);
+    
+
+    React.useLayoutEffect(() => {
+        const p = new THREE.Vector3(...position);
+        if(p.lengthSq() === 0) return;
+        const inward = p.clone().normalize().multiplyScalar(-1);
+        const from = new THREE.Vector3(0, -1, 0);
+        const q = new THREE.Quaternion().setFromUnitVectors(from, inward);
+        groupRef.current.quaternion.copy(q);
+    }, [position]);
 
     return (
-         <group>
-            <mesh ref={ref1} position={[0, 0, 0]}>
+         <group ref={groupRef} position={position} scale={[0.05, 0.05, 0.05]} rotation={[degToRad(180),0,0]}>
+            <mesh ref={refHead} position={[0, 0, 0]}>
                 <icosahedronGeometry args={[2, 5]} />
                 <meshStandardMaterial color="#FF0000" />
             </mesh>
 
-            <mesh ref={ref2} position={[0, -2, 0]} rotation={[radToDeg(180), 0 ,0]}>
+            <mesh ref={refCone} position={[0, -2, 0]} rotation={[0, 0 , degToRad(180)]}>
                 <coneGeometry args={[1, 4, 20]}/>
                 <meshStandardMaterial color="#FFFFFF" />
             </mesh>
